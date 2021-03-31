f23d905c370da0be2a113597fce5495df5d01f0f
sghebrab <g.bellicini003@studenti.unibs.it>
2021-03-31 22:14:49 +0200

    Now, after an encryption or decryption, the system tells you how long did it take to perform tha action.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from GOST import GOST
 import my_utils
+import time
 
 # gost = GOST()
 # msg = "This algorithm is probably the best one I ever implemented <3."
@@ -32,6 +33,7 @@ import my_utils
 
 gost = GOST()
 go_on = True
+error_message = "An error occurred while processing the data."
 while go_on:
     enc_or_dec = input("Type E to encrypt a message, D to decrypt one: ")
     if enc_or_dec.upper() == "E":
@@ -44,37 +46,43 @@ while go_on:
             key = my_utils.PBKDF2(password, salt)[0]
         else:
             key, salt = my_utils.PBKDF2(password, salt)
+        time_b = time.time()
         gost.set_message(my_utils.string_to_bytes(message))
         gost.set_key(key)
         gost.set_iv()
+        ciphertext = my_utils.leading_zeros_hex(gost.encrypt())
+        time_e = time.time()
         print("Data summary")
         print("Message: ", message)
         print("Password: ", password)
         print("Salt: ", salt)
         print("IV (hex): ", my_utils.leading_zeros_hex(gost.get_iv()))
-        ciphertext = my_utils.leading_zeros_hex(gost.encrypt())
         print("\nEncrypted message (hex): ", ciphertext, '\n')
+        print("Elapsed encryption time (s): ", time_e - time_b)
         file_or_no = input("Do you want to write the result to a file? (Y/N) ")
         if file_or_no.upper() == "Y":
             file_path = input("Type the full path to the file: ")
             if input("Do you want salt to be on the file? (Y/N) ").upper() == "Y":
                 my_utils.write_to_file(file_path, ciphertext, my_utils.leading_zeros_hex(gost.get_iv()), salt)
             else:
-                my_utils.write_to_file(ciphertext, my_utils.leading_zeros_hex(gost.get_iv()))
+                my_utils.write_to_file(file_path, ciphertext, my_utils.leading_zeros_hex(gost.get_iv()))
     else:
         ciphertext = input("Type the ciphertext: ")
         password = input("Type the PASSWORD you want to use: ")
         salt = input("Type the salt: ")
         iv = input("Type the IV: ")
+        time_b = time.time()
         key, salt = my_utils.PBKDF2(password, salt)
         gost.set_encrypted_msg(my_utils.hex_to_bin_mult_64(ciphertext))
         gost.set_key(key)
         gost.set_iv(my_utils.hex_to_bin_mult_64(iv))
+        plaintext = my_utils.bytes_to_string(gost.decrypt())
+        time_e = time.time()
         print("Data summary")
         print("Ciphertext (hex): ", ciphertext)
         print("Password: ", password)
         print("Salt: ", salt)
         print("IV (hex): ", iv)
-        plaintext = my_utils.bytes_to_string(gost.decrypt())
-        print("\nDecrypted message: ", plaintext, '\n')
+        print("\nDecrypted message: ", plaintext or error_message, '\n')
+        print("Elapsed decryption time (s): ", time_e - time_b)
     go_on = input("Continue? (Y/N) ").upper() == "Y"
