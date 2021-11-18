274bc8266ca5a979843b65f95feadeceb04a741e
sghebrab <g.bellicini003@studenti.unibs.it>
2021-11-18 11:44:34 +0100

    - Fixed leading_zeros_hex since it didn't do what it was supposed to do; - Added various comments here and there.

--- my_utils.py
+++ my_utils.py
@@ -4,15 +4,21 @@ import string
 from datetime import datetime
 
 
+# This function takes as input a string and returns another string where each original character has been replaced by
+# its UTF-8 encoding (e.g. "a" -> "01100001".
 def string_to_bytes(message):
     bin_array = bytearray(message, "utf8")
     byte_list = list()
     for b in bin_array:
+        # Since if b has leading zeros they are discarded when converting to bin, zfill(8) pads the bin number with as
+        # many 0s as needed to make it 8 characters long
         to_bin = bin(b)[2:].zfill(8)
         byte_list.append(to_bin)
     return ''.join(byte_list)
 
 
+# This function takes as input a string of bytes, i.e. a string containing only 0s and 1s, whose lenght is a multiple
+# of 8. It tries to perform UTF-8 encoding on each byte (or sequence of bytes for non-ASCII characters).
 def bytes_to_string(bytes_in):
     result = ""
     byte_list = list()
@@ -44,56 +50,41 @@ def bytes_to_string(bytes_in):
 
 
 def pbkdf2(password, salt=None):
+    # If salt is None, generate a random salt
     if salt is None:
         salt = random_salt()
+    # Derive the key by hashing the password along with salt
     key_bytes = hashlib.pbkdf2_hmac('sha256', bytes(password, 'utf-8'), bytes(salt, 'utf-8'), 10000)
-    # sha = hashlib.sha256()
-    # sha.update(bytes(password, 'utf-8'))
-    # sha_to_bytes = sha.digest()
     key = ""
+    # Since key_bytes is a list of bytes with length 32 (32*8 = 256 -> sha256), take every byte, convert it to bin, fill
+    # it with leading zeros and append the result to the whole key
     for i in range(0, 32):
         key = key + bin(key_bytes[i])[2:].zfill(8)
     return key, salt
 
 
-# def encode_base64(message):
-#     map = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P",
-#            "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f",
-#            "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
-#            "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "/"]
-#     encoded = []
-#     for i in range(len(message) // 6):
-#         char = message[i*6:(i+1)*6]
-#         char_to_num = int(char, 2)
-#         encoded.append(map[char_to_num])
-#     return ''.join(encoded)
-#
-#
-# def decode_base64(message):
-#     b64_map = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P",
-#            "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f",
-#            "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
-#            "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "/"]
-#     decoded = []
-#     for i in range(len(message)):
-#         char = b64_map.index(message[i])
-#         char_to_num = bin(char)[2:].zfill(6)
-#         decoded.append(char_to_num)
-#     return ''.join(decoded)
-
-
 def leading_zeros_hex(bin_message):
+    hex_chars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"]
     length = len(bin_message)
     if length % 64 != 0:
         final_length = (length // 64 + 1)*64
-        return hex(int(bin_message, 2))[2:].zfill(final_length)
-    return hex(int(bin_message, 2))[2:]
+        # Compute how many 0s are needed to make the message's length a multiple of 64
+        missing_zeros = final_length - length
+        # Append those 0s at the beginning of the string
+        bin_message = "0"*missing_zeros + bin_message
+    hex_msg = []
+    for i in range(0, len(bin_message), 4):
+        # Take 4 bits at a time, convert that value to int and index the hex_chars list to obtain the right character
+        idx = int(bin_message[i:i+4], 2)
+        hex_msg.append(hex_chars[idx])
+    return "".join(hex_msg)
 
 
 def random_salt(size=20):
     result = ""
     random.seed()
     for i in range(size):
+        # Take a random character between uppercase letters, lowercase letters, digits and punctuation characters
         rnd = random.randint(0, 3)
         if rnd == 0:
             result += random.choice(string.ascii_uppercase)
@@ -106,9 +97,9 @@ def random_salt(size=20):
     return result
 
 
-# This function takes as input a hex string and returns a binary strings whose length is
+# This function takes as input a hex string and returns a binary string whose length is
 # the minimum multiple of 64 above the length of the binarized hex string.
-# e.g. if hex string translates to 1101, then the result will be 62 zeros plus 1101
+# e.g. if hex string translates to 1101, then the result will be 60 zeros plus 1101
 def hex_to_bin_mult_64(hex_message):
     length = len(bin(int(hex_message, 16))[2:])
     if length % 64 != 0:
@@ -129,6 +120,7 @@ def write_to_file(path, ciphertext, iv, salt=None):
     print("Done writing!")
 
 
+# This is just to see how good the algorithm is at performing confusion.
 def odds_zeros_ones(bin_message):
     zeros = 0
     ones = 0
