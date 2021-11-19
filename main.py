fce9edee356cff0b39a5161269f06fb300fe9317
sghebrab <g.bellicini003@studenti.unibs.it>
2021-11-19 14:25:42 +0100

    - Minor improvements.

--- main.py
+++ main.py
@@ -14,15 +14,11 @@ print("Key: ", my_utils.leading_zeros_hex(key))
 print("Salt: ", salt)
 ciphertext = my_utils.leading_zeros_hex(gost.encrypt())
 print("IV: ", my_utils.leading_zeros_hex(gost.get_iv()))
-
 print("Encrypted: ", ciphertext)
-
-#deciphered = gost.decrypt(gost.CBC)
-#print("Decrypted: ", my_utils.bytes_to_string(deciphered))
 t2 = time.time()
 print("Elapsed time (s): ", t2 - t1)
 
-#Decrypt the ciphertext obtained before using a new GOST object
+# Decrypt the ciphertext obtained before using a new GOST object
 gost2 = GOST()
 key2 = my_utils.pbkdf2("Hallelujah", salt)[0]
 gost2.set_key(key2)
@@ -30,7 +26,6 @@ iv2 = my_utils.leading_zeros_hex(gost.get_iv())
 gost2.set_iv(my_utils.hex_to_bin_mult_64(iv2))
 gost2.set_encrypted_msg(my_utils.hex_to_bin_mult_64(ciphertext))
 gost2.set_operation_mode(gost2.CFB)
-
 print("Decrypted from scratch: ", my_utils.bytes_to_string(gost2.decrypt()))
 
 # gost = GOST()
